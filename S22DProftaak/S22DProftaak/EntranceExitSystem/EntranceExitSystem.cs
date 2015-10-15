40841a149b4d280090944ca3de1285b05089155b
mveltman <equentiz@hotmail.com>
2015-10-15 15:20:27 +0200

    skeleton methods entrance exit system

--- S22DProftaak/S22DProftaak/EntranceExitSystem/EntranceExitSystem.cs
+++ S22DProftaak/S22DProftaak/EntranceExitSystem/EntranceExitSystem.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using S22DProftaak.General;
 
 namespace S22DProftaak.InUitRijSysteem
 {
     class EntranceExitSystem
     {
+        public EntranceExitSystem()
+        {
+            throw new NotImplementedException
+        }
+        public bool EnterTrain(Train train, RailSection railsection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool getrails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetTrams()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ApplyRepairSession(RepairSystem repairsystem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveTram(Train train, RailSection railsection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateAllTrams()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
