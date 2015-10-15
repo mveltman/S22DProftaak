a4b26e8045a143904f9b44b2f97c99a801a4c6f5
Siebren Weertman <siebren.w@gmail.com>
2015-10-15 12:09:58 +0200

    Rail, section and Train
    Commit my life to this schitt

--- S22DProftaak/S22DProftaak/General/RailSection.cs
+++ S22DProftaak/S22DProftaak/General/RailSection.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace S22DProftaak.General
 {
+    /// <summary>
+    /// This is the class for railsection which is a section of rails.
+    /// This class holds information such as the position relative to other sections on a rail
+    /// And which train is on it(only one at the time)
+    /// </summary>
     class RailSection
     {
+        public int Position { get; private set; }
+        public bool Blocked { get; private set; }
+        public Train Train { get; private set; }
+
+        public RailSection(int position)
+        {
+            this.Position = position;
+
+        }//Sets position on rail
+        public bool RemoveTrain()
+        {
+            if(Train==null) return false;
+            else
+            {
+                Train = null;
+                return true;
+            }
+        }
+        public bool UpdateTrain(Train train)
+        {
+            if (Train == null) return false;
+            Train = train;
+            return true;
+        }
     }
 }
