d82575d07f155b3cd3c9e348632f20c4f167bcbc
erterer <sven.nottelman@live.nl>
2015-10-15 13:46:35 +0200

    Class user ingevuld
    Properties en constructor aangemaakt

--- S22DProftaak/S22DProftaak/General/User.cs
+++ S22DProftaak/S22DProftaak/General/User.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace S22DProftaak.General
 {
+    /// <summary>
+    /// This is the class for all users
+    /// This class holds all information about the user and their permissions
+    /// </summary>
     class User
     {
+        public UserTypeEnum Type { get; private set; }
+        public string UserName { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+
+        public User(UserTypeEnum type, string userName, string firstName, string lastName)
+        {
+            this.Type = type;
+            this.UserName = userName;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }  // sets the usertype, username, first and lastname
     }
 }
