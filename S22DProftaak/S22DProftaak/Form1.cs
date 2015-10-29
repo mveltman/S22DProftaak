8fb551937e51b06d2e3d2d6d46fdff7378f72697
Siebren Weertman <siebren.w@gmail.com>
2015-10-29 14:08:56 +0100

    Loginform

--- S22DProftaak/S22DProftaak/Form1.cs
+++ S22DProftaak/S22DProftaak/Form1.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using S22DProftaak.General;
 
 namespace S22DProftaak
 {
     public partial class Form1 : Form
     {
+        private GeneralSystem sys;
         public Form1()
         {
+            sys = new GeneralSystem();
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (Login(tbLogin.Text, tbPassword.Text))
+            {
+                throw new NotImplementedException();
+                //open new form, hide this form.
+                this.Hide();
+            }
+            // else has been handled in bool Login. Messagebox gets shown already. Should do nothing.
+        }
+
+        private bool Login(string name, string password)
+        {
+            string error = "";
+            if (!sys.Login(name, password, out error))
+            {
+                MessageBox.Show(error);
+                return false;
+            }
+            return true;
         }
     }
 }
