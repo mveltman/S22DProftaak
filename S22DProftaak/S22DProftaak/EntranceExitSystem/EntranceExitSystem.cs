c18f61a1316d0d221aa6e6cb4bd6f7b476d1319b
Siebren Weertman <siebren.w@gmail.com>
2015-10-29 15:26:25 +0100

    Some slight changes an creations of HORROR(forms)

--- S22DProftaak/S22DProftaak/EntranceExitSystem/EntranceExitSystem.cs
+++ S22DProftaak/S22DProftaak/EntranceExitSystem/EntranceExitSystem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using S22DProftaak.General;
-using S22DProftaak.RepairSystemForm;
+using S22DProftaak.RepairSystem;
 
 namespace S22DProftaak.InUitRijSysteem
 {
@@ -29,7 +29,7 @@ namespace S22DProftaak.InUitRijSysteem
             throw new NotImplementedException();
         }
 
-        public bool ApplyRepairSession(RepairSystemForm repairsystem)
+        public bool ApplyRepairSession(Action.Repair/*SystemForm*/ repairsystem)
         {
             throw new NotImplementedException();
         }
