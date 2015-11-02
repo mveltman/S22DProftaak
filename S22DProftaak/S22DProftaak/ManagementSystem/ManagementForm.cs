f76883a6d49c9d958f4b630d2c72128a435d20d8
mveltman <equentiz@hotmail.com>
2015-11-02 18:59:01 +0100

    lots of form updates

--- S22DProftaak/S22DProftaak/ManagementSystem/ManagementForm.cs
+++ S22DProftaak/S22DProftaak/ManagementSystem/ManagementForm.cs
@@ -7,17 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using S22DProftaak.General;
+using S22DProftaak.ManagementSystem;
 
 
 namespace S22DProftaak.ManagementSystem
 {
     public partial class ManagementForm : Form
     {
+        private string oldtext;
         public ManagementForm()
         {
             InitializeComponent();
+            
+        }
+        
+
+        private void ManagementForm_Load(object sender, EventArgs e)
+        {
 
         }
+
+
+
+        private void Textbox_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            RichTextBox currentBox = sender as RichTextBox;
+            oldtext = currentBox.Text;
+            if(e.Button == MouseButtons.Right)
+            {
+                //create new prompt here.
+
+            }
+            else if(e.Button == MouseButtons.Left)
+            {
+                currentBox.SelectAll();
+            }
+
+            currentBox.SelectAll();
+            currentBox.SelectionAlignment = HorizontalAlignment.Center;
+            
+        }
+
+        private void TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+
+
         
        
 
--- S22DProftaak/S22DProftaak/ManagementSystem/ManagementSystem.cs
+++ S22DProftaak/S22DProftaak/ManagementSystem/ManagementSystem.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using S22DProftaak.General;
+using System.Windows.Forms;
 
 namespace S22DProftaak.BeheerSysteem
 {
@@ -12,7 +13,12 @@ namespace S22DProftaak.BeheerSysteem
     /// </summary>
     class ManagementSystem
     {
+        private List<Train> trains;
         public ManagementSystem()
+        {
+            throw new NotImplementedException();
+        }
+        public bool GetTrains()
         {
             throw new NotImplementedException();
         }
@@ -35,6 +41,21 @@ namespace S22DProftaak.BeheerSysteem
         public bool ApplyForAction(Action action) //aanpassen
         {
             throw new NotImplementedException();
+        }
+        public bool ValidateNewInput(object sender, EventArgs e)
+        {
+            GetTrains();
+            RichTextBox currentbox = sender as RichTextBox;
+            foreach(Train t in trains)
+            {
+                //need to add trainnumber property to train. wait until sure nobody else did.
+               if(currentbox.Text == Convert.ToString(/*t.number*/))
+               {
+                   return true;
+               }
+
+            }
+            return false;
         }
     }
 }
